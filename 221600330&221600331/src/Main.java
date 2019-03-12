720da54b9cf847391218215636d0e0e90e41d54c
向鹏 <1055386758@qq.com>
2019-03-12 16:45:01 +0800

    Update Main.java

--- 221600330&221600331/src/Main.java
+++ 221600330&221600331/src/Main.java
@@ -1,4 +1,3 @@
-import java.io.*;
 import java.util.*;
 
 public class Main {
@@ -6,10 +5,14 @@ public class Main {
 	public static void main(String[] args) {
 		int countChar;
 		int countLinnes;
+		System.out.print("请输入文件名：");
+		Scanner scan = new Scanner(System.in);
+		String file_name = scan.nextLine();
+		scan.close();
 		Map<String, String> wordsMap;
-		countChar = wordCount.count_Characters("input.txt");
-		wordsMap = wordCount.count_Words("input.txt");
-		countLinnes = wordCount.count_Lines("input.txt");
+		countChar = wordCount.count_Characters(file_name);
+		wordsMap = wordCount.count_Words(file_name);
+		countLinnes = wordCount.count_Lines(file_name);
 		
 		wordCount.writeToFile(countChar,wordsMap,countLinnes);
 	}
